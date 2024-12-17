70664708c89049a5f9572415aa82345e1ed94c41
ap3ufersa <ap3ufersa@gmail.com>
2024-12-17 10:57:33 -0300

    Ajustando unidade 3

--- unidade2/ControleAluno.java
+++ unidade2/ControleAluno.java
@@ -8,13 +8,21 @@ public class ControleAluno {
         osQueridos = new ArrayList<>();
     }
 
-    /** Retorna verdadeiro se o aluno foi adicionado, falso c.c. */
+    /** Retorna verdadeiro se o aluno foi adicionado, falso caso contrário */
     public boolean adicionaAluno(Aluno novoAluno) {
+        if (novoAluno != null && !osQueridos.contains(novoAluno)) {
+            osQueridos.add(novoAluno);
+            return true;
+        }
         return false;
     }
 
-    /** Retorna verdadeiro se o aluno foi adicionado, falso c.c. */
+    /** Retorna verdadeiro se o aluno foi removido, falso caso contrário */
     public boolean removeAluno(Aluno aRemover) {
+        if (aRemover != null && osQueridos.contains(aRemover)) {
+            osQueridos.remove(aRemover);
+            return true;
+        }
         return false;
     }
 
@@ -30,5 +38,4 @@ public class ControleAluno {
     public String toString() {
         return "ControleAluno [osQueridos=" + osQueridos + "]";
     }
-
 }
--- unidade3/TestaAluno.java
+++ unidade3/TestaAluno.java
@@ -3,15 +3,19 @@ import java.util.LinkedList;
 public class TestaAluno {
     public static void main(String[] args) {
         LinkedList<Aluno> alunos = new LinkedList<>();
+        
+        System.out.println("");
+
+        try {
+            Aluno novoAluno = new Aluno("Aluno Invalido");
+            novoAluno.setNota1(-50);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         for (int i = 0; i < 99; i++) {
-            try{
-                Aluno novoAluno = new Aluno("Aluno" + (i + 1));
-                novoAluno.setNota1(-1);
-                alunos.add(novoAluno);
-            } catch (Exception e) {
-                e.getMessage();
-            }
+            Aluno novoAluno = new Aluno("Aluno" + (i + 1));
+            alunos.add(novoAluno);
         }
 
         System.out.println("Imprimindo os alunos:");
